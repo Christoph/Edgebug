aa29efc923d75a84ac2de1ef5c9044eb1b29805c
vadmin <vadmin@vadmin.(none)>
2009-12-04 17:10:48 +0100

    Deleted unused step definition.

--- features/step_definitions/edgebug_steps.rb
+++ features/step_definitions/edgebug_steps.rb
@@ -5,10 +5,3 @@ end
 Given /^I am on the (\w+) page/ do |page|
   visit "/#{page}"
 end
-
-Given /^I have a testcase "([^\"]*)" with the steps "([^\"]*)" and "([^\"]*)"$/ do |testcase_name, step1_name, step2_name|
-  raise testcase_name
-  testcase = Testcase.create(:title => testcase_name)
-  testcase.steps << Step.create(:description => step1_name)
-  testcase.steps << Step.create(:description => step2_name)
-end
