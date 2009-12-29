0f7138679a0bbf9a743b313f41593221ef515634
vadmin <vadmin@vadmin.(none)>
2009-12-29 15:58:10 +0100

    Show testcase title in testcase_results/show.

--- spec/views/testcase_results/show.html.haml_spec.rb
+++ spec/views/testcase_results/show.html.haml_spec.rb
@@ -4,13 +4,23 @@ describe "/testcase_results/show.html.haml" do
   include TestcaseResultsHelper
   
   before(:each) do
-    assigns[:testcase_result] = @testcase_result = Factory(:testcase_result)
+    @testcase = Factory(:testcase)
+    @testcase_result = Factory(:testcase_result)
+    @testcase_result.testcase = @testcase
+    
+    assigns[:testcase_result] = @testcase_result
   end
 
   it "should render attributes in <p>" do
     render "/testcase_results/show.html.haml"
     response.should have_text(/Testcase/)
     response.should have_text(/Result/)
+  end
+
+  it "should render the testcase title as h1" do
+    render "/testcase_results/show.html.haml"
+
+    response.should have_tag("h1", /#{@testcase.title}/)
   end
 end
 
