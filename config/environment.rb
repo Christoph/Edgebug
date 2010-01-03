51820c9c535269eb338ab579d9e74e2a2ae8e471
vadmin <vadmin@vadmin.(none)>
2010-01-03 22:12:28 +0100

    Added some gems to environment.rb and removed unused steps.

--- config/environment.rb
+++ config/environment.rb
@@ -12,15 +12,16 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "rspec-rails", :version => '=1.2.9', :lib => 'spec/rails'
-  ##config.gem "cucumber", :version => '=0.6.0' causes problem with rake
-  ##config.gem "cucumber-rails", :version => '=0.2.3'
+  config.gem "cucumber", :version => '=0.6.0'
+  ##config.gem "cucumber-rails", :version => '=0.2.3' #causes problem with rake
   config.gem "webrat", :version => '=0.6.0'
   config.gem "sqlite3-ruby", :lib => "sqlite3", :version => '=1.2.5' unless ENV["RAILS_ENV"] == "production"
   config.gem "haml", :version => '=2.2.16'
   config.gem "factory_girl", :lib => "factory_girl", :version => '>= 1.2.3'
   config.gem "faker", :version => '>= 0.3.1'
   config.gem "pickle", :lib => false, :version => ">=0.2.1"
-
+  config.gem "launchy", :version => "0.3.5"
+  config.gem "database_cleaner", :version => "=0.4.0"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
