87c24b42a7c86d8f7871bd07da12ce0ecd7afdb8
vadmin <vadmin@vadmin.(none)>
2010-01-07 17:43:16 +0100

    Create steps in seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-if RAILS_ENV == "production" then
-  return
-end
-
-10.times do
-  Testcase.create(:title => Faker::Lorem.words(rand(7)+1).join(' ').capitalize)
+if RAILS_ENV != "production" then
+  10.times do
+    testcase = Testcase.create(:title => Faker::Lorem.words(rand(7) + 1).join(' ').capitalize)
+    rand(8).times do
+      Teststep.create(:description => Faker::Lorem.paragraph, :testcase => testcase)
+    end
+  end
 end
