f8911d961b73896d652f0e9f1a52b5ed80fa237e
vadmin <vadmin@vadmin.(none)>
2010-01-07 19:00:14 +0100

    Changed root route to testcases.

--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,7 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "testcases"
 
   # See how all your routes lay out with "rake routes"
 
