47a8c3ea5e31923630058c4dccdf5a84e7c1fcb2
vadmin <vadmin@vadmin.(none)>
2010-01-21 19:28:04 +0100

    Fixed marking.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,26 @@
-function remove_fields(link) {
+function markItemAsSuccessful(id)
+{
+  id = "#successful_" + id
+  $(id).parent().parent().addClass("successful_teststep_result");
+  $(id).parent().children('input').value = "t";
+  $(id).addClass("successful_teststep_result");
+}
+
+function markItemAsFailed(id)
+{
+  id = "#failed_" + id
+  $(id).parent().parent().addClass("failed_teststep_result");
+  $(id).parent().children('input').value = "f";
+}
+
+function remove_fields(link)
+{
   $(link).prev("input[type=hidden]").val("1");
   $(link).closest(".fields").hide();
 }
 
-function add_fields(link, association, content) {
+function add_fields(link, association, content) 
+{
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
   $(link).parent().before(content.replace(regexp, new_id));
